d6d7fe88dcfd00931aa1ed1110ec1105cece6f8f
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-12 16:04:34 +0100

    Initial commit: HTML

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+// import { handleSearch } from './js/formSearch'
+
+
+import './styles/body.scss';
+import './styles/header.scss';
+import './styles/form.scss';
+import './styles/footer.scss';
+
+// export {
+//     handleSearch
+// };
