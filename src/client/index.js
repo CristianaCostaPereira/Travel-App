65c648123ee58beb8f53191290366c4eeffef24a
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-13 23:55:55 +0100

    File-loader and SCSS

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,10 @@
 // import { handleSearch } from './js/formSearch'
 
 
-// import './styles/body.scss';
-// import './styles/header.scss';
-// import './styles/form.scss';
-// import './styles/footer.scss';
+import './styles/body.scss';
+import './styles/header.scss';
+import './styles/form.scss';
+import './styles/footer.scss';
 
 // export {
 //     handleSearch
--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,20 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                    },
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    }
 }
