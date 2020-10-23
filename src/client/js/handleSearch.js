ba6507f71d66eef0ebed564658ad5543852ac247
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-23 19:02:06 +0100

    Modal WIP

--- src/client/js/handleSearch.js
+++ src/client/js/handleSearch.js
@@ -1,3 +1,6 @@
+import './modal.js'
+import { openModal } from './modal.js';
+
 const axios = require('axios');
 
 // Function to get the Form Info
@@ -23,32 +26,8 @@ const getTravelInfo = async (event) => {
 
     // Get image from pixabay
     document.getElementById("city-image").src = response.data.pictureURL;
-}
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    openModal();
 }
 
 // windou.print();
