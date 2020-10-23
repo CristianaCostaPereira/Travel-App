44d379f64b2d452c64460e75ada06e7d819292f2
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-23 19:02:27 +0100

    Modal WIP

--- src/client/js/modal.js
+++ src/client/js/modal.js
@@ -1,30 +1,22 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
+// When the user clicks the button, open the modal with the information wanted
 const openModal = () => {
     modal.style.display = "block";
 }
 const closeModal = () => {
     modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
