fb0bc7fff44ff8f38b20c9ba9871bc5c09a55e02
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-23 18:21:00 +0100

    Starting Modal

--- src/client/index.js
+++ src/client/index.js
@@ -7,6 +7,7 @@ import './styles/body.scss';
 import './styles/header.scss';
 import './styles/form.scss';
 import './styles/footer.scss';
+import './styles/modal.scss';
 
 export {
     getTravelInfo,
