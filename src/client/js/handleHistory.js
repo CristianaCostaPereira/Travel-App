355c38a0b5072595af15fde3d3cb3340eb6fa6c7
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-28 22:09:57 +0000

    Hide/show history cards

--- src/client/js/handleHistory.js
+++ src/client/js/handleHistory.js
@@ -1,32 +1,42 @@
 const axios = require('axios');
 
+// Variable that shows or hides the history cards
+let showHistory = false;
+
 // Make request to server
 // response.data is my array
 const travelHistory = async () => {
-    let response = await axios ({
-        method: 'GET',
-        url: '/travel-history'
-    });
+    if (showHistory == true) {
+        document.getElementById("travel-history-container").innerHTML = "";
+        showHistory = false
 
-    let historyCards = "";
-
-    response.data.forEach(envelope => {
-
-        historyCards += 
-            `<div class="history-card">
-                <h2>${envelope.cityName}</h2>
-                <div class="history-card-image">
-                    <img src="${envelope.tinyURL}" >
-                </div>
-                <span>${envelope.daysAway}</span>
-                <span><strong>${envelope.temperature}</strong></span>
-                <span>${envelope.weather}
-                    <i class="fas fa-cloud-sun"></i>
-                </span>
-            </div>`;
-    });
-
-    document.getElementById("travel-history-container").innerHTML = historyCards;
+    } else {
+        let response = await axios ({
+            method: 'GET',
+            url: '/travel-history'
+        });
+    
+        let historyCards = "";
+    
+        response.data.forEach(envelope => {
+    
+            historyCards += 
+                `<div class="history-card">
+                    <h2>${envelope.cityName}</h2>
+                    <div class="history-card-image">
+                        <img src="${envelope.tinyURL}" >
+                    </div>
+                    <span>${envelope.daysAway}</span>
+                    <span><strong>${envelope.temperature}</strong></span>
+                    <span>${envelope.weather}
+                        <i class="fas fa-cloud-sun"></i>
+                    </span>
+                </div>`;
+        });
+    
+        document.getElementById("travel-history-container").innerHTML = historyCards;
+        showHistory = true;
+    }
 }
 
 export { travelHistory }
